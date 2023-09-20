774df9fbf4fbd7fc748f7ab81b2a44ae551d8cb5
ely <96276919+elysiaojr@users.noreply.github.com>
2023-09-20 17:14:52 +0800

    changed layouts to material3
    - added library to gradle
    - changed create user layout

--- app/src/main/java/com/example/scannerapp/ui/CreateUserActivity.kt
+++ app/src/main/java/com/example/scannerapp/ui/CreateUserActivity.kt
@@ -11,14 +11,19 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.scannerapp.R
 import com.example.scannerapp.database.entities.User
 import com.example.scannerapp.viewmodels.UserViewModel
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class CreateUserActivity : AppCompatActivity() {
 
     private lateinit var userViewModel: UserViewModel
-    private lateinit var editTextName: EditText
-    private lateinit var switchStatus: Switch
-    private lateinit var buttonSave: Button
-
+    private lateinit var textInputLayoutName: TextInputLayout
+    private lateinit var textInputEditTextName: TextInputEditText
+    private lateinit var switchStatus: MaterialSwitch
+    private lateinit var buttonSave: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         // link this CreateUserActivity to the UI layout activity_create_user
         super.onCreate(savedInstanceState)
@@ -27,35 +32,36 @@ class CreateUserActivity : AppCompatActivity() {
         // initialise the ViewModel
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
-        editTextName = findViewById(R.id.editTextName)
+        textInputLayoutName = findViewById(R.id.textInputLayoutName)
+        textInputEditTextName = findViewById(R.id.textInputEditTextName)
         switchStatus = findViewById(R.id.switchStatus)
         buttonSave = findViewById(R.id.buttonSave)
 
         // only enable the "Save" button when there is text in the EditText
-        editTextName.addTextChangedListener {
-            val isNameEmpty = editTextName.text.toString().trim().isEmpty()
-            buttonSave.isEnabled = !isNameEmpty
-        }
-
+        // not sure why its not working :-( button is still enabled when there is no text
+//    textInputLayoutName.setEndIconOnClickListener  {
+//      val userName = textInputEditTextName.text.toString().trim()
+//      val isNameEmpty = userName.isEmpty()
+//      buttonSave.isEnabled = !isNameEmpty
+//    }
         buttonSave.setOnClickListener {
             saveUserToDatabase()
         }
+
     }
-
     private fun saveUserToDatabase() {
-        val userName = editTextName.text.toString().trim()
+        val userName = textInputEditTextName.text.toString().trim()
         val switchStatus = switchStatus.isChecked // true for enabled, false for disabled
-        var userStatus: Int // 1 for enabled, 0 for disabled
 
-        userStatus = if (switchStatus) {
+        val userStatus: Int = if (switchStatus) {
             1
         } else {
             0
-        }
+        } // 1 for enabled, 0 for disabled
 
         if (userName.isNotEmpty()) {
 
-            val newUser = User(name = userName, status = userStatus)
+            val newUser = User(userId = 0, name = userName, status = userStatus)
 
             // use the function in ViewModel to add the user
             userViewModel.addUser(newUser)
