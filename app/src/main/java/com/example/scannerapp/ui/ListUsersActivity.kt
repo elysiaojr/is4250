60c2a803cf280f3ff26bf3f1db587fdcb6c32288
micaellaa <99943347+micaellaa@users.noreply.github.com>
2023-09-21 19:33:36 +0800

    Basic list of users

--- app/src/main/java/com/example/scannerapp/ui/ListUsersActivity.kt
+++ app/src/main/java/com/example/scannerapp/ui/ListUsersActivity.kt
@@ -4,9 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.scannerapp.R
 
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
 class ListUsersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_users)
+
+        // Define Users
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+
+        // access the listView from xml file
+        var mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
     }
 }
