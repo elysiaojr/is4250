5916ff8a0dffe073aa817d4086c0f0efefe6b52f
micaellaa <99943347+micaellaa@users.noreply.github.com>
2023-09-23 01:31:03 +0800

    Added User Details page
    Can navigate from User List page -> User Details page

--- app/src/main/java/com/example/scannerapp/database/entities/UserEntity.kt
+++ app/src/main/java/com/example/scannerapp/database/entities/UserEntity.kt
@@ -2,6 +2,8 @@ package com.example.scannerapp.database.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import android.os.Parcel
+import android.os.Parcelable
 
 // Entity: User
 @Entity(tableName = "user")
@@ -10,4 +12,32 @@ data class User(
   val userId: Int = 1,
   val name: String,
   val status: Int
-)
+) : Parcelable {
+  constructor(parcel: Parcel) : this(
+    parcel.readInt(),
+    parcel.readString() ?: "",
+    parcel.readInt()
+    // Initialize other properties here
+  )
+
+  override fun writeToParcel(parcel: Parcel, flags: Int) {
+    parcel.writeInt(userId)
+    parcel.writeString(name)
+    parcel.writeInt(status)
+    // Write other properties to the parcel
+  }
+
+  override fun describeContents(): Int {
+    return 0
+  }
+
+  companion object CREATOR : Parcelable.Creator<User> {
+    override fun createFromParcel(parcel: Parcel): User {
+      return User(parcel)
+    }
+
+    override fun newArray(size: Int): Array<User?> {
+      return arrayOfNulls(size)
+    }
+  }
+}
---
+++ app/src/main/java/com/example/scannerapp/ui/UserDetailsActivity.kt
@@ -0,0 +1,71 @@
+package com.example.scannerapp.ui
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.scannerapp.R
+import com.example.scannerapp.database.entities.User
+import com.example.scannerapp.databinding.ActivityUserDetailsBinding
+
+class UserDetailsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityUserDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_user_details)
+
+        // Retrieve the selected user's data from the intent extras
+        //val user = intent.getSerializableExtra("user") as User
+
+        // Display user details in the activity's UI
+        //val nameTextView = findViewById<TextView>(R.id.userNameTextView)
+        //nameTextView.text = user.name
+
+        // Retrieve the selected user's data from the intent extras
+        val user = intent.getParcelableExtra<User>("user")
+
+        val userNameTextView = findViewById<TextView>(R.id.userNameTextView)
+        val userStatusTextView = findViewById<TextView>(R.id.userStatusTextView)
+        // Populate the user's name and status in the TextViews
+        user?.let {
+            userNameTextView.text = it.name
+            val statusParsed = getStatusText(it.status)
+            userStatusTextView.text = statusParsed
+        }
+
+        // Find the back button by its ID
+        val backButton = findViewById<ImageView>(R.id.backButton)
+
+        // Set a click listener for the back button
+        backButton.setOnClickListener {
+            handleOnBackPressed() // This will navigate back to the previous screen
+        }
+        // Display user details in the activity's UI
+        //val nameTextView = findViewById<TextView>(R.id.userNameTextView)
+        //nameTextView.text = user.name
+    }
+
+    // Handle the back button press
+    private fun handleOnBackPressed() {
+        super.onBackPressed()
+        // Optionally, you can add additional logic here if needed
+    }
+
+    private fun getStatusText(status: Int): String {
+        // You can customize this function to map status values to text
+        return when (status) {
+            0 -> "Inactive"
+            1 -> "Active"
+            else -> "Unknown"
+        }
+    }
+}
