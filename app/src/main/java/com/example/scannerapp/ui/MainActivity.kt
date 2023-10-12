d77cf08aed9a80b614790adfc8c793f6e5b976bd
ely <96276919+elysiaojr@users.noreply.github.com>
2023-10-12 14:41:20 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/scannerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/scannerapp/ui/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.scannerapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.scannerapp.R
 import com.example.scannerapp.theme.ScannerAppTheme
+import com.journeyapps.barcodescanner.CaptureActivity
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         ) {
 //          insertIntoDatabase()
           Greeting("Group 7")
+          BarcodeScanner()
         }
       }
     }
@@ -42,6 +50,39 @@ class MainActivity : AppCompatActivity() {
 //    val user = User(name = "Justin", status = 0)
 //    mUserViewModel.addUser(user)
 //  }
+
+  private val barcodeLauncher =
+    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      if (result.resultCode == RESULT_OK) {
+        val data = result.data
+        val scanResult = data?.getStringExtra("SCAN_RESULT")
+        if (scanResult == null) {
+          Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+        } else {
+          Toast.makeText(this, "Scanned: $scanResult", Toast.LENGTH_LONG).show();
+        }
+      } else {
+        // Handle the case when scanning was canceled or failed.
+        Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show();
+      }
+    }
+
+  // Composable function for barcode scanning
+  @Composable
+  private fun BarcodeScanner() {
+    val context = this
+    Button(
+      onClick = {
+        val scanOptions = ScanOptions() // Customize scan options as needed
+        val intent = Intent(context, CaptureActivity::class.java)
+        intent.action = "com.google.zxing.client.android.SCAN"
+        barcodeLauncher.launch(intent)
+      },
+      modifier = Modifier.padding(16.dp)
+    ) {
+      Text(text = "Scan Barcode")
+    }
+  }
 
   @Composable
   fun Greeting(name: String, modifier: Modifier = Modifier) {
