9af0ee16d76be3d95f69f66af43ec55fd2231f27
micaellaa <99943347+micaellaa@users.noreply.github.com>
2023-10-13 08:56:38 +0800

    Consumable Dropdown Search

--- app/src/main/java/com/example/scannerapp/ui/CreateRecordDialog.kt
+++ app/src/main/java/com/example/scannerapp/ui/CreateRecordDialog.kt
@@ -1,5 +1,6 @@
 package com.example.scannerapp.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,7 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ListView
+import android.widget.PopupWindow
 import androidx.appcompat.widget.AppCompatSpinner
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.scannerapp.R
+import com.example.scannerapp.adapters.ConsumableSearchAdapter
 import com.example.scannerapp.ui.ui.ui.theme.ScannerAppTheme
 import com.example.scannerapp.viewmodels.RecordViewModel
 import com.google.android.material.button.MaterialButton
@@ -40,63 +46,91 @@ class CreateRecordDialog : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         // initialise the ViewModel
         recordViewModel = ViewModelProvider(this).get(RecordViewModel::class.java)
+        //consumableListView = findViewById<ListView>(R.id.consumableDropdownList)
 
-        // Initialize and set up views and listeners here
-        val closeButton = view.findViewById<Button>(R.id.closeButton)
-        val textInputEditTextItem = view.findViewById<TextInputEditText>(R.id.textInputEditTextItem)
-        val textInputEditTextBrand =
-            view.findViewById<TextInputEditText>(R.id.textInputEditTextBrand)
-        val textInputEditTextType = view.findViewById<TextInputEditText>(R.id.textInputEditTextType)
-        val textInputEditTextSize = view.findViewById<TextInputEditText>(R.id.textInputEditTextSize)
-        val textInputEditTextNameItemCode =
-            view.findViewById<TextInputEditText>(R.id.textInputEditTextNameItemCode)
-        val spinnerUOM = view.findViewById<AppCompatSpinner>(R.id.spinnerUOM)
-        val textInputEditTextNamePerUnitQuantity =
-            view.findViewById<TextInputEditText>(R.id.textInputEditTextNamePerUnitQuantity)
-        val textInputEditTextNameMinQuantity =
-            view.findViewById<TextInputEditText>(R.id.textInputEditTextNameMinQuantity)
-        val switchStatus = view.findViewById<MaterialSwitch>(R.id.switchStatus)
-        val saveButton = view.findViewById<MaterialButton>(R.id.buttonSave)
+//        // Initialize and set up views and listeners here
+//        val closeButton = view.findViewById<Button>(R.id.closeButton)
+//        val textInputEditTextItem = view.findViewById<TextInputEditText>(R.id.textInputEditTextItem)
+//        val textInputEditTextBrand =
+//            view.findViewById<TextInputEditText>(R.id.textInputEditTextBrand)
+//        val textInputEditTextType = view.findViewById<TextInputEditText>(R.id.textInputEditTextType)
+//        val textInputEditTextSize = view.findViewById<TextInputEditText>(R.id.textInputEditTextSize)
+//        val textInputEditTextNameItemCode =
+//            view.findViewById<TextInputEditText>(R.id.textInputEditTextNameItemCode)
+//        val spinnerUOM = view.findViewById<AppCompatSpinner>(R.id.spinnerUOM)
+//        val textInputEditTextNamePerUnitQuantity =
+//            view.findViewById<TextInputEditText>(R.id.textInputEditTextNamePerUnitQuantity)
+//        val textInputEditTextNameMinQuantity =
+//            view.findViewById<TextInputEditText>(R.id.textInputEditTextNameMinQuantity)
+//        val switchStatus = view.findViewById<MaterialSwitch>(R.id.switchStatus)
+//        val saveButton = view.findViewById<MaterialButton>(R.id.buttonSave)
+        val consumablesAutoCompleteTextView = view.findViewById<AutoCompleteTextView>(R.id.consumablesAutoCompleteTextView) // Consumables search dropdown
 
-        // Close the dialog when the close button is clicked
-        closeButton.setOnClickListener {
-            dismiss()
+//        // Close the dialog when the close button is clicked
+//        closeButton.setOnClickListener {
+//            dismiss()
+//        }
+
+//        // Retrieve the string array from resources
+//        val uomValues = resources.getStringArray(R.array.uom_values)
+//
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        val adapter =
+//            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, uomValues)
+
+        // Consumables search dropdown
+        val dropdownListView = view.findViewById<ListView>(R.id.consumableDropdownList)
+        val consumableSearchAdapter = ConsumableSearchAdapter(requireContext(), R.id.consumableDropdownList, emptyList())
+        val dropdownContainer = view.findViewById<FrameLayout>(R.id.dropdownContainer)
+
+        // Create the PopupWindow
+        val popupWindow = PopupWindow(dropdownContainer, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true)
+
+        dropdownListView.adapter = consumableSearchAdapter
+        consumablesAutoCompleteTextView.setAdapter(consumableSearchAdapter)
+        // Set an item click listener to handle selection
+
+        consumablesAutoCompleteTextView.setOnItemClickListener { _, _, position, _ ->
+            val selectedConsumable = consumableSearchAdapter.getItem(position)
+            consumablesAutoCompleteTextView.setText(selectedConsumable.consumableName)
+            // Handle the selected item as needed
         }
 
-        // Retrieve the string array from resources
-        val uomValues = resources.getStringArray(R.array.uom_values)
+        // Create a popup window for the ScrollView
+        val inflater
+        val dropdownContainer = inflater.inflate(R.layout.consumables_search_dropdown_component, null)
+        val scrollView = dropdownContainer.findViewById<ScrollView>(R.id.scrollView)
+        val listView = scrollView.findViewById<ListView>(R.id.consumableDropdownList)
+        // End of Consumable search dropdown
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        val adapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, uomValues)
 
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // Set the adapter to the Spinner
-        spinnerUOM.adapter = adapter
-
-        var selectedUOM = ""
-        spinnerUOM.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parentView: AdapterView<*>?,
-                selectedItemView: View?,
-                position: Int,
-                id: Long
-            ) {
-                // Handle item selection here
-                selectedUOM = uomValues[position]
-                Log.d("SpinnerSelection", "Selected UOM: $selectedUOM")
-            }
-
-            override fun onNothingSelected(parentView: AdapterView<*>?) {
-                // Handle no selection
-                spinnerUOM.prompt = "Select a Unit of Measurement"
-            }
-        }
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//
+//        // Set the adapter to the Spinner
+//        spinnerUOM.adapter = adapter
+//
+//        var selectedUOM = ""
+//        spinnerUOM.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(
+//                parentView: AdapterView<*>?,
+//                selectedItemView: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                // Handle item selection here
+//                selectedUOM = uomValues[position]
+//                Log.d("SpinnerSelection", "Selected UOM: $selectedUOM")
+//            }
+//
+//            override fun onNothingSelected(parentView: AdapterView<*>?) {
+//                // Handle no selection
+//                spinnerUOM.prompt = "Select a Unit of Measurement"
+//            }
+//        }
 
         // Handle save button click
 //        saveButton.setOnClickListener {
