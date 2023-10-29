0458270d9b43d49bb2f0c8946a628e7fd3740256
ChavChavC <teezongzhengjustin@gmail.com>
2023-10-29 20:55:16 +0800

    Formatting changes

--- app/src/main/java/com/example/scannerapp/ui/EditRecordsDialog.kt
+++ app/src/main/java/com/example/scannerapp/ui/EditRecordsDialog.kt
@@ -1,5 +1,19 @@
 package com.example.scannerapp.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.scannerapp.R
+import com.example.scannerapp.database.entities.Record
+import com.example.scannerapp.viewmodels.BatchDetailsViewModel
+import com.example.scannerapp.viewmodels.RecordViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
 //import android.os.Bundle
 //import android.view.LayoutInflater
 //import android.view.View
@@ -42,3 +56,31 @@ package com.example.scannerapp.ui
 //        return inflater.inflate(R.layout.dialog_edit_record, container, false)
 //    }
 //}
+class EditRecordsDialog(private var record: Record) :
+    DialogFragment(), CoroutineScope {
+    private val job = Job()
+    private lateinit var batchDetailsViewModel: BatchDetailsViewModel
+    private lateinit var recordViewModel: RecordViewModel
+    var recordUpdatedListener: onRecordsUpdatedListener? = null
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    interface onRecordsUpdatedListener {
+        fun onRecordUpdated(record: Record)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Set dialog style
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+    }
+
+    override fun OnCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_edit_record, container, false)
+    }
+}
